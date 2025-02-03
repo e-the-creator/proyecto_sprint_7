a3a18bc27706ed4929f8a4b0551cfa8ca76aa57f
Emilio Cruz <emiliocruzbarrientos@gmail.com>
2025-02-02 18:28:06 -0600

    Carga de archivos para Aplicación

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # proyecto_sprint_7
 Desarrollo de Aplicación web
+3:00 pm Creación de archivo requirements.txt
+3:10 pm Descarga de archivo vehicles_us.csv
+3:17 pm Creación de Directorio notebooks
+3:20 pm Creación de archivo EDA.ipynb en el directorio notebooks
+4:00 pm Modificación de archivo EDA
+4:15 pm Archivo EDA muestra histograma mediante plotly express
+4:30 pm Modificación de archivo EDA
+4:50 pm Creación de gráfico de dispersión en archivo EDA
+5:00 pm Creación de archivo app.py en directorio raíz
+5:20 pm Creaión de app basada en streamlit
+6:20 pm Carga de los archivos modificados
+6:25 pm Push a Git
---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+
+st.header('Histograma de Anuncios de venta de Coches', divider = 'gray')
+
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+st.download_button(
+    label = "Descargar Dataset",
+    data = car_data.to_csv(index = False),
+    file_name = "vehicles_us.csv"
+)
+
+
+
+       
